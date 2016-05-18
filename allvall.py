af87be372171fdf95cb263fbc2281d2c5dfae850
kp14 <kp14@ebi>
2016-05-18 15:35:33 +0100

    Reset to original state.

--- allvall.py
+++ allvall.py
@@ -22,32 +22,6 @@ def run(path):
             subprocess.call(CMD.format(prog=prog,
                                        seq1=combi[0],
                                        seq2=combi[1]), shell=True)
-    with open('water_alignments.txt', 'r', encoding='ascii') as wres, open('test.svg', 'w', encoding='ascii') as svg:
-        aln = None
-        alignments = []
-        for line in wres:
-            if line.startswith('# Program:'):
-                aln = {}
-            elif line.startswith('# 1'):
-                seq1 = line.strip().split(':')[1]
-            elif line.startswith('# 2'):
-                seq2 = line.strip().split(':')[1]
-            elif line.startswith('# Identity'):
-                identity = line.strip()[23:-2]
-            elif line.startswith('# Similarity'):
-                similarity = line.strip()[23:-2]
-            elif line.startswith('#---'):
-                alignments.append(aln)
-        for alignment in alignments:
-            svg.write('''<svg width="40" height="40">
-                         <rect width="40" height="40" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-                         </svg> ''')
-
-def _svg_rect(num):
-    template = '<rect x="{x}" y="{y}" width="40" height="40" style="fill:rgb(0,0,255);fill-opacity:{opa}" />'
-
-
-
 
 
 if __name__ == "__main__":
