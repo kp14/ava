b626cb493052449550145c3c82c71f80ecc15c72
kp14 <kp14@ebi>
2016-05-20 14:06:39 +0100

    Fix bugs in order of seqs and SVg generation.

--- allvall.py
+++ allvall.py
@@ -10,7 +10,7 @@ import logging
 import os
 import subprocess
 import sys
-from collections import defaultdict, namedtuple, OrderedDict
+from collections import defaultdict, namedtuple
 
 logging.basicConfig(level=logging.INFO)
 
@@ -28,10 +28,11 @@ Partner = namedtuple('Partner', ['seq', 'identity', 'similarity'])
 def run_alignments(path):
     cleanup(path)
     files = glob.glob(os.path.join(path, '*.fasta'))
+    files.sort(key=lambda path: _filename_no_ext(path))
     cnt = defaultdict(int)
     alignments_run = 0
     combis_run = 0
-    for combi in itertools.combinations(files, 2):
+    for combi in itertools.combinations_with_replacement(files, 2):
         logging.debug(combi)
         combis_run += 1
         cnt[combi[0]] += 1
@@ -51,13 +52,16 @@ def generate_svg(alignments, index, path):
     with open(svg_file, 'w', encoding='utf8') as svg:
         total = 0
         svg.write('<svg xmlns:svg="http://www.w3.org/2000/svg" width="1000" height="1000">')
-        sequences_ordered = OrderedDict.fromkeys(index)
-        for yidx, seq in enumerate(sequences_ordered.keys()):
-            for xidx, partner in enumerate(alignments[seq]):
-                x = str(X_INIT + (xidx * WIDTH) + GAP)
-                y = str(Y_INIT + (yidx * HEIGHT) + GAP)
+        index_dict = {}
+        for idx, val in enumerate(index):
+            index_dict[val] = idx
+        for seq in index:
+            for partner in alignments[seq]:
+                x = str(X_INIT + (index_dict[seq] * WIDTH) + GAP)
+                y = str(Y_INIT + (index_dict[partner.seq] * HEIGHT) + GAP)
                 opa = partner.identity
-                svg.write(_svg_rect(x, y, opa))
+                title = '{0}-{1}'.format(seq, partner.seq)
+                svg.write(_svg_rect(x, y, opa, title))
                 logging.debug('Added <rect> at {x}, {y} with opacity {opa}'.format(x=x, y=y, opa=opa))
                 total += 1
         logging.info('Number of <rect> written: {}'.format(str(total)))
@@ -68,7 +72,7 @@ def generate_svg(alignments, index, path):
 def parse_alignment_output(alignment_file, path):
     with open(os.path.join(path, alignment_file), 'r', encoding='ascii') as wres:
         alignments = defaultdict(list)
-        index = []
+        index = set()
         counter = 0
         seq1 = None
         seq2= None
@@ -85,11 +89,13 @@ def parse_alignment_output(alignment_file, path):
                 seq2 = line.strip().split(':')[1]
                 logging.debug('Seq 2: {}'.format(seq2))
             elif line.startswith('# Identity'):
-                txt_id = line.strip()[24:-2]
+                id_tokens = line.strip().split()
+                txt_id = id_tokens[-1][1:-2].strip()
                 identity = str(round(float(txt_id) / 100, 2))
                 logging.debug('Identity: {}'.format(identity))
             elif line.startswith('# Similarity'):
-                txt_sim = line.strip()[24:-2]
+                sim_tokens = line.strip().split()
+                txt_sim = sim_tokens[-1][1:-2].strip()
                 similarity = str(round(float(txt_sim) / 100, 2))
                 logging.debug('Similarity: {}'.format(similarity))
             elif line.startswith('#--'):
@@ -97,14 +103,17 @@ def parse_alignment_output(alignment_file, path):
                     alignment_partner = Partner(seq2, identity, similarity)
                     alignments[seq1].append(alignment_partner)
                     logging.debug(seq1, alignment_partner)
-                    index.append(seq1)
+                    index.add(seq1)
                     seq1 = None
                     seq2= None
                     identity = None
                     similarity = None
         logging.info('Length of alignment index: {}'.format(str(len(index))))
         logging.info('Counted alignments: {}'.format(str(counter)))
-        return (alignments, index)
+        for k, v in alignments.items():
+            alignments[k].sort(key=lambda partner: partner.seq)
+        sorted_index = sorted(list(index))
+        return (alignments, sorted_index)
 
 
 def cleanup(path):
@@ -117,19 +126,25 @@ def cleanup(path):
             except:
                 logging.error('Cannot clean target dir of output file: {}'.format(file))
 
-def _svg_rect(x, y, opa):
+
+def _svg_rect(x, y, opa, title):
     if opa == '1.0':
         color = 'green'
     else:
         color = 'blue'
-    template = '<g><rect x="{x}" y="{y}" width="{w}" height="{h}" style="fill:{color};fill-opacity:{opa}" /></g>'
-    return template.format(x=x, y=y, w=WIDTH, h=HEIGHT, color=color, opa=opa)
+    template = '<g><title>{title}</title><rect x="{x}" y="{y}" width="{w}" height="{h}" style="fill:{color};fill-opacity:{opa}" /></g>'
+    return template.format(x=x, y=y, w=WIDTH, h=HEIGHT, color=color, opa=opa, title=title)
 
+
+def _filename_no_ext(path):
+    fname = os.path.basename(path)
+    name_no_ext = os.path.splitext(fname)
+    return name_no_ext
 
 def run():
     path = sys.argv[1]
     run_alignments(path)
-    alignments, index = parse_alignment_output('water_alignments.txt', path)
+    alignments, index = parse_alignment_output('needle_alignments.txt', path)
     generate_svg(alignments, index, path)
 
 
